8d9d9f5fcd9e666b06c42dd99cb5ab80b6098781
Tómas <tomashg1985@gmail.com>
2021-10-30 11:56:31 +0000

    scaffolding added

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello world!'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(child: Text('I Am Rich')),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg'),
+          ),
+        ),
       ),
     ),
   );
