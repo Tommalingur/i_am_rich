e225d1e79ea580f1443def3374d96900ab4ce217
Tómas <tomashg1985@gmail.com>
2021-10-30 17:14:16 +0000

    assets added

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,18 @@ import 'package:flutter/material.dart';
 
 // The main Function is the starting point for all our Flutter apps.
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: Center(child: Text('I Am Rich')),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: Center(
-          child: Image(
-            image: NetworkImage(
-                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg'),
-          ),
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Center(child: Text('I Am Rich')),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
         ),
       ),
     ),
-  );
+  ));
 }
